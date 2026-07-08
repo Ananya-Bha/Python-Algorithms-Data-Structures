5ce4b8be5ff75be0197d8b13142237145e71e936
Ananya Bhargava <ananya.bhargava.sa@gmail.com>
2026-07-08 11:16:49 +0200

    Add ANSI colour and style escape codes

---
+++ 06. Advanced Design & ASCII Art/Basic ANSI Colour & Style Escape codes (1).py
@@ -0,0 +1,26 @@
+# FOREGROUND (text) colour escape codes
+k = "\033[30m"  # black
+r = "\033[31m"  # red
+g = "\033[32m"  # green
+y = "\033[33m"  # yellow
+b = "\033[34m"  # blue
+p = "\033[35m"  # purple
+c = "\033[36m"  # cyan
+w = "\033[37m"  # white
+reset = "\033[0m"  # reset text back to normal
+
+# BACKGROUND colour escape codes
+kb = "\033[40m"  # black
+rb = "\033[41m"  # red
+gb = "\033[42m"  # green
+yb = "\033[43m"  # yellow
+bb = "\033[44m"  # blue
+pb = "\033[45m"  # purple
+cb = "\033[46m"  # cyan
+wb = "\033[47m"  # white
+
+# escape codes for styles
+bold = "\033[1m" 
+faint = "\033[2m"     
+underline = "\033[4m"
+invert = "\033[7m"
